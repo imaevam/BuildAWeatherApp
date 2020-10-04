b3d7e3429351fd76fdbe1ebad5046833b9c8ae29
imaevam <imaevam@list.ru>
2020-10-04 20:32:57 +0300

    class User and methods

--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy 
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 db = SQLAlchemy()
 
@@ -11,3 +13,22 @@ class News(db.Model): # атрибуты, поля в таблице
 
     def __repr__(self): # магический метод, чтобы в дальнейшем при получении обьекта понимать что это за объект
         return '<News {} {}>'.format(self.title, self.url)
+
+class User(db.Model, UserMixin): #множественное наследование; def is_authenticated и тд
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True) # ограничение по длине; те, колонки по которым в дальнейшем будем фильтровать запросы имеет смысл делать индексами, т.к. поиск по индексу быстрее
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password) #true \ false
+
+    def __repr__(self):
+        return 'User {}'.format(self.username)
+
+    
+
+

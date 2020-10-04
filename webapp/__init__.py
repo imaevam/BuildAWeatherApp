a30120e327c44bbcfebbb148695d9358893b9644
imaevam <imaevam@list.ru>
2020-10-04 19:45:43 +0300

    first commit

---
+++ webapp/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+from webapp.forms import LoginForm
+from webapp.model import db, News
+from webapp.weather import weather_by_city
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        title = "Новости Python"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news_list = News.query.order_by(News.published.desc()).all() # верни нам все новости из БД 
+        return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    return app
---
+++ webapp/model.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
+
+class News(db.Model): # атрибуты, поля в таблице
+    id = db.Column(db.Integer, primary_key=True) # целое число, первичный ключ, бд будет его индексировать
+    title = db.Column(db.String, nullable=False) # nullable -может ли это значение не быть в сопоставляемых данных, просим БД проверять нас
+    url = db.Column(db.String, unique=True, nullable=False) #unique - url у каждой новости уникальный
+    published = db.Column(db.DateTime, nullable=False)
+    text = db.Column(db.Text, nullable=True)
+
+    def __repr__(self): # магический метод, чтобы в дальнейшем при получении обьекта понимать что это за объект
+        return '<News {} {}>'.format(self.title, self.url)

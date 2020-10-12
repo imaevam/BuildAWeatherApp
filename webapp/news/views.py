5602b212c01da69b4d812e6d860b5db50389763c
imaevam <imaevam@list.ru>
2020-10-12 19:28:25 +0500

    adding main menu

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -10,5 +10,5 @@ def index():
     title = "Новости Python"
     weather = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
     news_list = News.query.order_by(News.published.desc()).all() # верни нам все новости из БД 
-    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+    return render_template('news/index.html', page_title=title, weather=weather, news_list=news_list)
     

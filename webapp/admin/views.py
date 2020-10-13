ffa51b187b8107ffc6e713ca0b60dcfd53344c9a
imaevam <imaevam@list.ru>
2020-10-13 17:06:17 +0500

    decorators

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,13 +1,11 @@
-from flask_login import current_user, login_required
-from flask import Blueprint
+from webapp.user.decorators import admin_required
+from flask import Blueprint, render_template
 
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 @blueprint.route('/')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Привет, админ!'
-    else:
-        return 'Ты не админ!'
+    title = 'Панель управления'
+    return render_template('admin/index.html', page_title=title)

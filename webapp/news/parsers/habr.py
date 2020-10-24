71318dc657f25da3062ce4eba3f095d8024a7a8b
imaevam <imaevam@list.ru>
2020-10-24 18:05:45 +0300

    локаль для распознавания даты

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -1,7 +1,29 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
+import locale
+import platform
 
 from webapp.news.parsers.utils import get_html, save_news
+
+
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, "russian")  # русскоязычная локаль для распознавания даты
+else: 
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+
+def parse_habr_date(date_str):
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    elif 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
+
 
 def get_habr_snippets():
     html = get_html("https://habr.com/ru/search/?target_type=posts&q=python&order_by=date")
@@ -12,4 +34,5 @@ def get_habr_snippets():
             title = news.find('a', class_='post__title_link').text
             url = news.find('a', class_='post__title_link')['href']
             published = news.find('span', class_='post__time').text
-            print(title, url, published)
+            published = parse_habr_date(published)
+            save_news(title, url, published)

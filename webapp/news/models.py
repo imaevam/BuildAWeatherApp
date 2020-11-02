05fa934bc5f540f4d7731554ae46e6890fc690e4
imaevam <imaevam@list.ru>
2020-11-02 13:12:50 +0300

    запросы и связанных страниц

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,11 +1,39 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from webapp.db import db
 
+
 class News(db.Model): # атрибуты, поля в таблице
-    id = db.Column(db.Integer, primary_key=True) # целое число, первичный ключ, бд будет его индексировать
-    title = db.Column(db.String, nullable=False) # nullable -может ли это значение не быть в сопоставляемых данных, просим БД проверять нас
-    url = db.Column(db.String, unique=True, nullable=False) #unique - url у каждой новости уникальный
+    id = db.Column(db.Integer, primary_key=True)  # целое число, первичный ключ, бд будет его индексировать
+    title = db.Column(db.String, nullable=False)  # nullable -может ли это значение не быть в сопоставляемых данных, просим БД проверять нас
+    url = db.Column(db.String, unique=True, nullable=False)  # unique - url у каждой новости уникальный
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
-    def __repr__(self): # магический метод, чтобы в дальнейшем при получении обьекта понимать что это за объект
+    def comments_count(self):
+        return Comment.query.filter(Comment.news_id == self.id).count()
+
+    def __repr__(self):  # магический метод, чтобы в дальнейшем при получении обьекта понимать, что это за объект
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),  # поведение поля при удалении новости
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
+
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -15,6 +15,7 @@ def login():
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
 
+
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
     form = LoginForm()
@@ -29,11 +30,13 @@ def process_login():
     flash("Неправильные имя или пароль")
     return redirect(url_for("user.login"))
 
+
 @blueprint.route('/logout')
 def logout():
     logout_user()
     flash('Вы успешно покинули личную страницу')
     return redirect(url_for('news.index'))
+
 
 @blueprint.route('/register')
 def register():
@@ -42,6 +45,7 @@ def register():
     form = RegistrationForm()
     title = 'Регистрация'
     return render_template('user/registration.html', page_title=title, form=form)
+
 
 @blueprint.route('/process-reg', methods=['POST'])
 def process_reg():
